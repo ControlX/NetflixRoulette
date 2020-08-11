930e9b322b5f91f44bc8b0f797cfc486d1ced660
Abhishek Verma <abhishek@iMac.local>
2020-08-12 00:57:45 +0530

    Understanding of Webpack, Babel and Environment Variable's configurations

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+module.exports = {
+    mode: 'production',
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+       filename: 'build.js',
+       path: path.join(__dirname, '/prod/dist')},
+    module:{
+       rules:[{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env',
+                      '@babel/react',{
+                      'plugins': ['@babel/plugin-proposal-class-properties']}]
+        }
+       }]
+    },
+    plugins:[
+       new HWP(
+          {template: path.join(__dirname,'/src/index.html')}
+       )
+    ]
+}
