3fdd1cc12da115a47134525dc14c2ea0492d5e9d
Abhishek Verma <abhishek.verma.in@gmail.com>
2020-10-17 19:14:33 +0530

    Removed code duplicacy and integrated with Forms feedback. Pending to pull Redux feedback from Redux branch

--- src/components/AddEditMovie/AddEditMovie.jsx
+++ src/components/AddEditMovie/AddEditMovie.jsx
@@ -14,117 +14,68 @@ export default function AddEditMovie(props) {
         formik.handleSubmit();
     }
 
-    const formik = (props.editMovieSelection !== undefined) ?
+    function validate() {
+        const errors = {};
+        // Added the touched to avoid the validator validating all fields at once
+        if ((formik.touched.title && !formik.values.title) || (isSubmitButton && !formik.values.title)) {
+            errors.title = "*Title is a required field";
+        }
+
+        if ((formik.touched.release_date && !isValidDate(formik.values.release_date)) || (isSubmitButton && !isValidDate(formik.values.release_date))) {
+            errors.release_date = "*Not a valid Release Date. Please enter in YYYY-MM-DD format.";
+        }
+
+        if ((formik.touched.poster_path && !formik.values.poster_path) || (isSubmitButton && !formik.values.poster_path)) {
+            errors.poster_path = "*Movie URL is a required field";
+        } else if ((formik.touched.poster_path && !isValidUrl(formik.values.poster_path)) || (isSubmitButton && !isValidUrl(formik.values.poster_path))) {
+            errors.poster_path = "*Not a valid Movie URL";
+        }
+
+        if ((formik.touched.overview && !formik.values.overview) || (isSubmitButton && !formik.values.overview)) {
+            errors.overview = "*Overview is a required field";
+        }
+
+        if ((formik.touched.genres && !formik.values.genres) || (isSubmitButton && !formik.values.genres)) {
+            errors.genres = "*Genres is a required field";
+        }
+
+        if ((formik.touched.runtime && !formik.values.runtime) || (isSubmitButton && !formik.values.runtime)) {
+            errors.runtime = "*Runtime is a required field";
+        }
+        else if ((formik.touched.runtime && !isNumber(formik.values.runtime)) || (isSubmitButton && !isNumber(formik.values.runtime))) {
+            errors.runtime = "Runtime accepts only numbers";
+        }
+
+        if (isSubmitButton) {
+            boxRef.current.scrollIntoView();
+            isSubmitButton = false;
+        }
+
+        return errors;
+    }
+
+    const formik = (props.editMovieSelection.id !== undefined) ?
         useFormik({
             initialValues: props.editMovieSelection,
-            validate() {
-                console.log("Inside validate", formik.values, isSubmitButton)
-                const errors = {};
-                // Added the touched to avoid the validator validating all fields at once
-                if ((formik.touched.title && !formik.values.title) || (isSubmitButton && !formik.values.title)) {
-                    errors.title = "*Title is a required field";
-                }
-
-                if ((formik.touched.release_date && !isValidDate(formik.values.release_date)) || (isSubmitButton && !isValidDate(formik.values.release_date))) {
-                    errors.release_date = "*Not a valid Release Date. Please enter in YYYY-MM-DD format.";
-                }
-
-                if ((formik.touched.poster_path && !formik.values.poster_path) || (isSubmitButton && !formik.values.poster_path)) {
-                    errors.poster_path = "*Movie URL is a required field";
-                } else if ((formik.touched.poster_path && !isValidUrl(formik.values.poster_path)) || (isSubmitButton && !isValidUrl(formik.values.poster_path))) {
-                    errors.poster_path = "*Not a valid Movie URL";
-                }
-
-                if ((formik.touched.overview && !formik.values.overview) || (isSubmitButton && !formik.values.overview)) {
-                    errors.overview = "*Overview is a required field";
-                }
-
-                if ((formik.touched.genres && !formik.values.genres) || (isSubmitButton && !formik.values.genres)) {
-                    errors.genres = "*Genres is a required field";
-                }
-
-                if ((formik.touched.runtime && !formik.values.runtime) || (isSubmitButton && !formik.values.runtime)) {
-                    errors.runtime = "*Runtime is a required field";
-                }
-                else if ((formik.touched.runtime && !isNumber(formik.values.runtime)) || (isSubmitButton && !isNumber(formik.values.runtime))) {
-                    errors.runtime = "Runtime accepts only numbers";
-                }
-
-                if (isSubmitButton) {
-                    boxRef.current.scrollIntoView();
-                    isSubmitButton = false;
-                }
-
-                return errors;
-            },
+            validate,
             onSubmit(values) {
                 if (!Array.isArray(values.genres))
                     values.genres = (values.genres).split(',');
                 values.runtime = Number(values.runtime);
-                console.log("Form values: ", values);
                 props.onEditMovieSaveAction(values);
             }
         }) :
         useFormik({
-            initialValues: {
-                title: "",
-                release_date: "",
-                poster_path: "",
-                genres: "",
-                overview: "",
-                runtime: ""
-            },
-
-            validate() {
-                console.log("Inside validate", formik.values, isSubmitButton)
-                const errors = {};
-                // Added the touched to avoid the validator validating all fields at once
-                if ((formik.touched.title && !formik.values.title) || (isSubmitButton && !formik.values.title)) {
-                    errors.title = "*Title is a required field";
-                }
-
-                if ((formik.touched.release_date && !isValidDate(formik.values.release_date)) || (isSubmitButton && !isValidDate(formik.values.release_date))) {
-                    errors.release_date = "*Not a valid Release Date. Please enter in YYYY-MM-DD format.";
-                }
-
-                if ((formik.touched.poster_path && !formik.values.poster_path) || (isSubmitButton && !formik.values.poster_path)) {
-                    errors.poster_path = "*Movie URL is a required field";
-                } else if ((formik.touched.poster_path && !isValidUrl(formik.values.poster_path)) || (isSubmitButton && !isValidUrl(formik.values.poster_path))) {
-                    errors.poster_path = "*Not a valid Movie URL";
-                }
-
-                if ((formik.touched.overview && !formik.values.overview) || (isSubmitButton && !formik.values.overview)) {
-                    errors.overview = "*Overview is a required field";
-                }
-
-                if ((formik.touched.genres && !formik.values.genres) || (isSubmitButton && !formik.values.genres)) {
-                    errors.genres = "*Genres is a required field";
-                }
-
-                if ((formik.touched.runtime && !formik.values.runtime) || (isSubmitButton && !formik.values.runtime)) {
-                    errors.runtime = "*Runtime is a required field";
-                }
-                else if ((formik.touched.runtime && !isNumber(formik.values.runtime)) || (isSubmitButton && !isNumber(formik.values.runtime))) {
-                    errors.runtime = "Runtime accepts only numbers";
-                }
-
-                if (isSubmitButton) {
-                    boxRef.current.scrollIntoView();
-                    isSubmitButton = false;
-                }
-
-                return errors;
-            },
+            initialValues: props.editMovieSelection,
+            validate,
             onSubmit(values) {
                 values.genres = (values.genres).split(',');
                 values.runtime = Number(values.runtime);
-                console.log("Form values: ", values);
                 props.onAddMovieSubmitAction(values);
             }
         })
 
     return (
-        (props.editMovieSelection !== undefined) ?
             <>
                 <div id="myModal" className="modal">
                     <div className="modal-content">
@@ -132,11 +83,15 @@ export default function AddEditMovie(props) {
                             <span className="close" onClick={() => props.onCloseAction()}>&times;</span>
                         </div>
                         <div className="modal-body">
-                            <label className="modal-header-lbl-properties">EDIT MOVIE</label>
+                            {(props.editMovieSelection.id !== undefined) ? <label className="modal-header-lbl-properties">EDIT MOVIE</label> : <label className="modal-header-lbl-properties">ADD MOVIE</label>}
                             <br /><br />
-                            <form id="form-edit-movie" onSubmit={formik.handleSubmit}>
+                            <form id={(props.editMovieSelection.id !== undefined) ? "form-edit-movie" : "form-add-movie"} onSubmit={formik.handleSubmit}>
+                                {(props.editMovieSelection.id !== undefined) ?
+                                <>
                                 <p className="modal-body-titles">MOVIE ID</p>
                                 <input className="modal-body-input-disabled" placeholder="" value={formik.values.id} onChange={(e) => handleTitleChange(e)} disabled={true} />
+                                </> :
+                                null }
                                 <p className="modal-body-titles" ref={boxRef}>TITLE</p>
                                 <input type="title" name="title" id="title" className="modal-body-input" autoComplete="off" placeholder="Movie Title" value={formik.values.title} onBlur={formik.handleBlur} onChange={formik.handleChange} />
                                 <p className="modal-error-msg">{formik.errors.title ? formik.errors.title : null}</p>
@@ -159,54 +114,29 @@ export default function AddEditMovie(props) {
                         </div>
                         <div className="modal-footer">
                             <button type="reset" className="modal-btn-reset" onClick={formik.handleReset}>RESET</button>
-                            <button type="submit" className="modal-btn-submit" onClick={submitRequest}>SAVE</button>
+                            <button type="submit" className="modal-btn-submit" onClick={submitRequest}>{(props.editMovieSelection.id !== undefined) ? "SAVE" : "SUBMIT"}</button>
                         </div>
                     </div>
                 </div>
-            </> :
-            <>
-                <div id="myModal" className="modal">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <span className="close" onClick={() => props.onCloseAction()}>&times;</span>
-                        </div>
-
-                        <div className="modal-body">
-                            <label className="modal-header-lbl-properties">ADD MOVIE</label>
-                            <br /><br />
-                            <form id="form-add-movie" onSubmit={formik.handleSubmit}>
-                                <p className="modal-body-titles" ref={boxRef}>TITLE</p>
-                                <input type="title" name="title" id="title" className="modal-body-input" autoComplete="off" placeholder="Movie Title" value={formik.values.title} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.title ? formik.errors.title : null}</p>
-                                <p className="modal-body-titles" >RELEASE DATE</p>
-                                <input type="release_date" name="release_date" id="release_date" className="modal-body-input" autoComplete="off" placeholder="Select Date" value={formik.values.release_date} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.release_date ? formik.errors.release_date : null}</p>
-                                <p className="modal-body-titles" >MOVIE URL</p>
-                                <input type="poster_path" name="poster_path" id="poster_path" className="modal-body-input" autoComplete="off" placeholder="Movie URL here" value={formik.values.poster_path} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.poster_path ? formik.errors.poster_path : null}</p>
-                                <p className="modal-body-titles" >GENRE</p>
-                                <input type="genres" name="genres" id="genres" className="modal-body-input" autoComplete="off" placeholder="Select Genre" value={formik.values.genres} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.genres ? formik.errors.genres : null}</p>
-                                <p className="modal-body-titles" >OVERVIEW</p>
-                                <input type="overview" name="overview" id="overview" className="modal-body-input" autoComplete="off" placeholder="Overview here" value={formik.values.overview} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.overview ? formik.errors.overview : null}</p>
-                                <p className="modal-body-titles" >RUNTIME</p>
-                                <input type="runtime" name="runtime" id="runtime" className="modal-body-input" autoComplete="off" placeholder="Runtime here" value={formik.values.runtime} onBlur={formik.handleBlur} onChange={formik.handleChange} />
-                                <p className="modal-error-msg">{formik.errors.runtime ? formik.errors.runtime : null}</p>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="reset" className="modal-btn-reset" onClick={formik.handleReset}>Reset</button>
-                            <button type="submit" onClick={submitRequest} className="modal-btn-submit">Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </>
+            </> 
     )
 }
 
+AddEditMovie.defaultProps = {
+    editMovieSelection : {
+        title: "",
+        release_date: "",
+        poster_path: "",
+        genres: "",
+        overview: "",
+        runtime: ""
+    }
+}
 
-//============ For later reference
+
+
+
+//============ For later reference =========//
     // var expanded = false;
 
     // function showCheckboxes() {
